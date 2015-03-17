05edfab6c5f4eaaf4f1b28b4e90729fa28bc0184
Amos Wenger <fasterthanlime@gmail.com>
2015-03-17 17:40:25 +0100

    travis things

--- samples/prime.js
+++ samples/prime.js
@@ -24,7 +24,7 @@ function primeTest() {
     var res = [];
 
     print('Have native helper: ' + (primeCheckHelper !== primeCheckEcmascript));
-    for (var i = 1; i < 1000000; i++) {
+    for (var i = 1; i < 100000; i++) {
         if (primeCheck(i) && (i % 10000) == 9999) { res.push(i); }
     } 
     print(res.join(' '));
